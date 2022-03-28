8c0afe0a1888372cb58ff1ee92903d8de106c4f8
Leonardo Miniussi <leonardo.miniussi@leonardo.miniussi>
2022-03-28 18:31:39 -0300

    Initial commit

---
+++ test/financial_management/core_test.clj
@@ -0,0 +1,7 @@
+(ns financial-management.core-test
+  (:require [clojure.test :refer :all]
+            [financial-management.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
